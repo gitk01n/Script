69bc16fc9dd302a36dcfb906a1a1b81a799fd2fa
gitk01n <83260491+gitk01n@users.noreply.github.com>
2021-05-30 22:51:56 +0800

    Update 52pojie.js

--- 52pojie.js
+++ 52pojie.js
@@ -31,11 +31,11 @@ QuantumultX 远程脚本配置:
 
 [task_local]
 # 吾爱签到
-0 9 * * * https://raw.githubusercontent.com/gitk01n/Script/main/52pojie.js
+0 9 * * * https://raw.githubusercontent.com/gitk01n/Script/master/52pojie.js
 
 [rewrite_local]
 # 获取Cookie
-https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/gitk01n/Script/main/52pojie.js
+https:\/\/www\.52pojie\.cn\/home\.php\? url script-request-header https://raw.githubusercontent.com/gitk01n/Script/master/52pojie.js
 
 [mitm] 
 hostname= www.52pojie.cn
